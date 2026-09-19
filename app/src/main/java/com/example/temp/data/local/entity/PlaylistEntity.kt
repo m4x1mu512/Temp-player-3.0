753f46aa9653f504611efb2821d24de5963b4bc0
m4x1mu512 <trickster1212@xmail.ru>
2026-09-19 18:12:03 +0400

    Create data/local/entity/PlaylistEntity.kt

---
+++ app/src/main/java/com/example/temp/data/local/entity/PlaylistEntity.kt
@@ -0,0 +1,12 @@
+package com.example.temp.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "playlists")
+data class PlaylistEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String,
+    val createdAt: Long = System.currentTimeMillis(),
+    val coverUri: String? = null
+)
