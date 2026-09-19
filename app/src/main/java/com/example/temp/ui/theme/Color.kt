bf0726b8aa7a05249338ad967220c46ab4a1127d
m4x1mu512 <trickster1212@xmail.ru>
2026-09-19 18:20:32 +0400

    Create ui/theme/Color.kt

---
+++ app/src/main/java/com/example/temp/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.temp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightGreenBackground = Color(0xFFF5F9F5)
+val LightGreenSurface = Color(0xFFE8F0E8)
+
+val AccentBlue = Color(0xFF4FC3F7)
+val AccentTurquoise = Color(0xFF26C6DA)
+val AccentPurple = Color(0xFFAB47BC)
+
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+
+val NeonGreen = Color(0xFF00FF88)
+val NeonBlue = Color(0xFF00D4FF)
+val NeonPurple = Color(0xFFBB86FC)
